b0bbc5dfceba90288604f5acb3ae3439f464a8cc
SevenStars <32127313+NoTabaco@users.noreply.github.com>
2021-03-05 21:50:02 +0900

    Updated get my own nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,24 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
-
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "desc")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
